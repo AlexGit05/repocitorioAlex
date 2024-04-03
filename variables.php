2356fdaeb75d80c55547fa901ed42544c9b25119
alumno <alexjmarquezsel@gmail.com>
2024-04-03 09:22:28 -0300

    como usar variables

---
+++ variables.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>variables</title>
+</head>
+<body>
+    <?php
+$variable1= 'esta es la primera variable'; //texto puede ser con comillas simples o dobles (string)
+$nombre= 'alex';
+$edad= 18; // enteros (integer)
+$altura= 1.77; //decimales, usando el punto en lugar de la coma (float)
+$mayorEdad= true; // verdad o falso (boolean)
+
+
+?>
+</body>
+</html>
