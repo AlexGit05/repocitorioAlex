9365ebae454ce2971c20d6b0d3cc329f71073a7b
alumno <alexjmarquezsel@gmail.com>
2024-04-03 08:48:40 -0300

    como usar comentarios

---
+++ comentarios.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>coment</title>
+</head>
+<body>
+    <?php
+    //esto es un comentario de una linea
+    echo 'wazaaa';
+    /* esto es un comentario multi linea
+    mira aqi esta otra linea del mismo comentario*/
+    echo ' que lees? este archivo es para probar comentarios';
+    # este es al estilo consola de una linea 
+    echo ' es toda we bye bye ';
+    ?>
+</body>
+</html>
