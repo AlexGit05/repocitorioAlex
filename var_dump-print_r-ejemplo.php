b04f8d6589cce7473c53eb83f8abed42ac2b081f
alexander <alexjmarquezsel@gmail.com>
2024-04-17 07:53:05 -0300

    var_dump y print_r

---
+++ var_dump-print_r-ejemplo.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $edad = 17;
+    $nombre = "alexander";
+    var_dump($edad);
+    var_dump($nombre);
+    echo "<br>";
+    print_r ($edad);
+    print_r ($nombre);
+    ?>
+
+</body>
+</html>
