32330a6d6270b151fd082bd86833ba345c35a939
Alexander Marquez <alexjmarquezsel@gmal.com>
2024-04-23 17:52:04 -0300

    ejercicio verduleria

--- problema1.1.php
+++ problema1.1.php
@@ -4,44 +4,48 @@
     <link rel="stylesheet" type="text/css" href="problema1.1.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Consumo de Frutas</title>
 </head>
 <body>
-    <h3> Verduleria Pepe </h3>
-<br>
-<br>
-<br>
-<br>
-    <h1> Consumo final </h1>
-    <p> A continuación se detallan los totales de su compra</p>
-    <?php 
-    $bananas=2;
-    $manzanas=1;
-    $bananaskg=100;
-    $manzanaskg=200;
-
-    $totalbananas=$bananas*$bananaskg;
-    $totalmanzana=$manzanas*$manzanaskg;
-    $total=$totalbananas+$totalmanzana;
-    ?>
-
     <div id="a1">
-<ul><li>El precio del kilo de banana es: <?php echo $bananaskg;?></li>
-<li>El precio del kilo de manzana es: <?php echo $manzanaskg;?></li></ul>
-</div>
+        <h3>Verduleria Pepe</h3>
+        <h1>Consumo final</h1>
+        <div class="info">
+            <p>El precio del kilo de banana es: $100</p>
+            <p>El precio del kilo de manzana es: $200</p>
+        </div>
+        <p>A continuación ingresa las cantidades de bananas y manzanas:</p>
 
-<div id="a2">
-<ul><li><?php echo $bananas;?> kg de bananas $<?php echo $bananaskg;?></li>
-<li><?php echo $manzanas;?> kg de manzana $<?php echo $manzanaskg;?></li></ul>
-<echo>Total: <?php echo $total; ?> 
-</div>
+        <form method="post" action="">
+            <label for="bananas">Cantidad de kilos de bananas:</label>
+            <input type="number" id="bananas" name="bananas" min="0" required>
+            <br>
+            <label for="manzanas">Cantidad de kilos de manzanas:</label>
+            <input type="number" id="manzanas" name="manzanas" min="0" required>
+            <br>
+            <button type="submit">Calcular Total</button>
+        </form>
 
+        <?php
+        if (isset($_POST["bananas"]) && isset($_POST["manzanas"])) {
+            $bananas = $_POST["bananas"];
+            $manzanas = $_POST["manzanas"];
+            $bananaskg = 100;
+            $manzanaskg = 200;
 
-    
-
-   
- 
-   
-
+            $totalbananas = $bananas * $bananaskg;
+            $totalmanzanas = $manzanas * $manzanaskg;
+            $total = $totalbananas + $totalmanzanas;
+        ?>
+        </div>
+        <div id="a2">
+            <ul>
+                <li><?php echo $bananas; ?> kg de bananas $<?php echo $totalbananas; ?></li>
+                <li><?php echo $manzanas; ?> kg de manzana $<?php echo $totalmanzanas; ?></li>
+                <li>Total: $<?php echo $total; ?></li>
+            </ul>
+        </div>
+        <?php } ?>
+    </div>
 </body>
 </html>
